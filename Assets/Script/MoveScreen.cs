9fe63aba7805baa627e948e00305afc1554d62b6
vungocthy <ngocthyvu1107@gmail.com>
2023-07-11 14:45:44 +0700

    Add

---
+++ Assets/Script/MoveScreen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveScreen : MonoBehaviour
+{
+
+    /*public string mainSceneName;
+    public string loadingSceneName;
+    public float loadingTime = 3f;
+
+    private void Start()
+    {
+        StartCoroutine(LoadMainScene());
+    }
+
+    private IEnumerator LoadMainScene()
+    {
+        yield return new WaitForSeconds(loadingTime);
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(mainSceneName);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }*/
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+}
+
+
